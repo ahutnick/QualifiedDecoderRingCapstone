e23163f88de767d666987876d7eb3bc449b4b1b6
Tasha Hutnick <ahutnick@sas.upenn.edu>
2022-05-27 14:53:07 -0400

    add upper / lowercase test

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -6,25 +6,31 @@ describe("Caesar", () => {
     it("returns false with a zero shift", () => {
         const zero = caesar("Try this", 0);
         expect(zero).to.be.false;
-    })
+    });
 
     it("returns false with a shift below -25", () => {
         const negTwentySix = caesar("Try this", -26);
         expect(negTwentySix).to.be.false;
-    })
+    });
 
     it("returns false with a shift above 25", () => {
         const twentySix = caesar("Try This", 26);
         expect(twentySix).to.be.false;
-    })
+    });
 
     it("wraps around the end of the alphabet", () => {
         const zShiftThree = caesar("z", 3);
         expect(zShiftThree).to.equal("c");
-    })
+    });
 
     it("wraps around the beginning of the alphabet", () => {
         const aShiftThree = caesar("a", -3);
         expect(aShiftThree).to.equal("x");
-    })
+    });
+
+    it("works the same with capital and lowercase letters", () => {
+        const upper = caesar("TRY THIS", 3);
+        const lower = caesar("try this", 3);
+        expect(upper).to.equal(lower);
+    });
 })
