eff5d1959ef38029183bfe5d02a79dd8432e2cad
Tasha Hutnick <ahutnick@sas.upenn.edu>
2022-05-27 15:50:12 -0400

    finish decode functionality

--- src/polybius.js
+++ src/polybius.js
@@ -6,8 +6,50 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
+  function polybiusBoard() {
+    return  [
+              ["a", "b", "c", "d", "e"], 
+              ["f", "g", "h", "i/j", "k"],
+              ["l", "m", "n", "o", "p"],
+              ["q", "r", "s", "t", "u"],
+              ["v", "w", "x", "y", "z"]
+            ];
+  }
+
+  function parseDecrypt(input) {
+    const board = polybiusBoard();
+    let message = "";
+    for(let i = 0; i < input.length; i += 2) {
+      const colPos = input[i] - 1;
+      const rowPos = input[i + 1] - 1;
+      const letter = board[rowPos][colPos];
+      message += letter;
+    }
+    return message;
+  }
+  
+  function decrypt(input) {
+    let message = "";
+    if (input.includes(" ")) {
+      const phrases = input.split(" ");
+      for (let phrase in phrases) {
+        if (phrases[phrase].length % 2 != 0) return false;
+        message += `${parseDecrypt(phrases[phrase])} `;
+      }
+      message = message.trim();
+    } else {
+      if (input.length % 2 != 0) return false;
+      message = parseDecrypt(input);
+    }
+    return message;
+  }
+
+  function encrypt(input) {
+
+  };
+
   function polybius(input, encode = true) {
-    // your solution code here
+    return (encode) ? encrypt(input) : decrypt(input);
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,28 @@
 // Write your tests here!
+const polybius = require("../src/polybius")["polybius"];
+const expect = require("chai").expect;
+
+describe("Polybius", () => {
+    describe("Encrypt", () => {
+
+    })
+
+    describe("Decrpyt", () => {
+        it("returns false when given an odd amount of numbers", () => {
+            const odd = polybius("11 223", false);
+            expect(odd).to.be.false;
+        });
+
+        it("has proper alphabet assignment", () => {
+            const alphabet = polybius("11213141511222324252132333435314243444541525354555", false);
+            expect(alphabet).to.equal("abcdefghi/jklmnopqrstuvwxyz");
+        });
+
+        it("keeps spaces where they are", () => {
+            const message = polybius("11 21 31 41 51", false);
+            expect(message).to.equal("a b c d e");
+        })
+
+
+    })
+})
